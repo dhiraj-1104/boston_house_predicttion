d569133ac09ba7f03f375462191d59c420ad4331
Dhiraj <dhirajchavan@1104>
2025-06-21 15:52:27 +0530

    Web Application Completed

--- app.py
+++ app.py
@@ -21,12 +21,20 @@ def home():
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
     new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
     output=regmodel.predict(new_data)
-    print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    try:
+        data=[float(x) for x in request.form.values()]
+        final_input=scalar.transform(np.array(data).reshape(1,-1))
+        print(final_input)
+        output = regmodel.predict(final_input)
+        return render_template("home.html",predicted_text="The House Price Prediction is {}".format(output))
+    except ValueError:
+        return render_template("home.html", prediction_text="Error: Please enter only numeric values.")
 
 if __name__=="__main__":
     app.run(debug=True)
